0795bc60ec4762b367ab2dcc3861420ec3dfffcf
Zoltán Garamvölgyi <garazoli@gmail.com>
2021-08-02 20:59:46 +0200

    5. Feladat beküldése

---
+++ testproject/periodic_table.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+try:
+    # Oldal betöltése
+    driver.get("https://witty-hill-0acfceb03.azurestaticapps.net/periodic_table.html")
+    time.sleep(3)
+
+
+    with open('data.txt', 'r') as f:
+        line = f.readline()
+        periodic_list = f.readline().split('/n'))
+
+
+    # Sajna lejárt az időm...
+finally:
+    pass
